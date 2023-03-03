4597456eb153f6794a37033e0343fe3828fc93ee
jared <jach2355@gmail.com>
2023-03-03 17:27:03 +0800

    preprocessing refactor into collection script v1

--- fpga/training_data_collector.py
+++ fpga/training_data_collector.py
@@ -2,6 +2,9 @@ import pandas as pd
 from intcomm import IntComm
 import time
 
+def preprocess_data(df):
+    return df
+
 if __name__ == "__main__":
 
     # change this according to serial port
@@ -36,12 +39,29 @@ if __name__ == "__main__":
                         [yaw, pitch, roll, accx, accy, accz, flex1, flex2]
                     )
 
+            # Convert data to DataFrame
+            df = pd.DataFrame(all_data)
+            df.columns = ["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"]
+
+            # Show user the data and prompt for confirmation
+            print(df.head())
+            confirmation = input("Does the data look ok? (y/n): ")
+            if confirmation.lower() == 'y':
+                # Preprocess data and send to function
+                processed_df = preprocess_data(df)
+                # Do something with the processed data here...
+                print(processed_df.head())
+            else:
+                # Clear data and start again
+                all_data = []
+                print("Starting again...")
+
     except KeyboardInterrupt:
         print("terminating program")
     except Exception:
         print("an error occured")
 
-    df = pd.DataFrame(all_data)
-    print(df.head())
-    df.columns = ["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"]
-    df.to_csv("training_data.csv", sep=",")
+    # df = pd.DataFrame(all_data)
+    # print(df.head())
+    # df.columns = ["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"]
+    # df.to_csv("training_data.csv", sep=",")
