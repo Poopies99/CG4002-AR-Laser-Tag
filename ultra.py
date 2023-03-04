00d21db8d5fdb1cd7c749f866f39489cf25d1e3f
Poopies99 <>
2023-03-04 14:20:28 +0800

    Made Changes

--- ultra.py
+++ ultra.py
@@ -394,6 +394,13 @@ class Training(threading.Thread):
     def run(self):
         unpacker = BLEPacket()
         all_data = []
+        variables = ['Acc-X', 'Acc-Y', 'Acc-Z', 'Gyro-X', 'Gyro-Y', 'Gyro-Z', 'Flex1', 'Flex2']
+        factors = ['mean', 'variance', 'median_absolute_deviation', 'root_mean_square', 'interquartile_range',
+                   'percentile_75', 'kurtosis', 'min_max', 'signal_magnitude_area', 'zero_crossing_rate',
+                   'spectral_centroid', 'spectral_entropy', 'spectral_energy', 'principle_frequency']
+
+        headers = [f'{var}_{factor}' for var in variables for factor in factors]
+
         while not self.shutdown.is_set():
             try:
                 data = fpga_queue.get()
@@ -416,8 +423,7 @@ class Training(threading.Thread):
                     )
 
                 # Convert data to DataFrame
-                df = pd.DataFrame(all_data)
-                df.columns = ["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"]
+                df = pd.DataFrame([data], columns=["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"])
 
                 # Show user the data and prompt for confirmation
                 print(df.head())
@@ -430,6 +436,7 @@ class Training(threading.Thread):
                 # Append processed data to CSV file
                 with open("processed_data.csv", "a") as f:
                     writer = csv.writer(f)
+                    writer.writerow(headers)
                     writer.writerow(processed_data)
 
                 # Clear raw data list
