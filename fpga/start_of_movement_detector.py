ed5dcf0d8ecf2f2c2e4a24b5e27bd65920557e51
jared <jach2355@gmail.com>
2023-03-05 15:52:37 +0800

    start of movement v1.2

--- fpga/start_of_movement_detector.py
+++ fpga/start_of_movement_detector.py
@@ -10,32 +10,35 @@ x[30:35] = 10
 
 # Define the window size and threshold factor
 window_size = 7
-threshold_factor = 3
+threshold_factor = 2
 
 # Apply median filtering with a dynamic threshold
 filtered = np.zeros_like(x)
+threshold = np.zeros_like(x)
 
 # Compute moving window median
 for i in range(window_size // 2, len(x) - window_size // 2):
     filtered[i] = np.median(x[i - window_size // 2:i + window_size // 2 + 1])
 
-# Compute threshold using past median data
-threshold = threshold_factor * np.median(np.abs(filtered - np.roll(filtered, window_size // 2)))
+    # Compute threshold using past median data, threshold = mean + k * std
+    past_filtered = filtered[i - window_size // 2:i + window_size // 2]
+    threshold[i] = np.mean(past_filtered) + (threshold_factor * np.std(past_filtered))
+    print(f"{i}, {filtered[i]}, {threshold[i]}\n")
 
 # Identify movement
 movement_detected = []
 for i in range(window_size // 2, len(x) - window_size // 2):
-    if np.abs(filtered[i] - filtered[i-1]) > threshold:
+    if filtered[i] > threshold[i]:
         movement_detected.append(t[i])
 
 # Output individual datapoint values to a CSV file
-df = pd.DataFrame({'time': t, 'original': x, 'threshold': threshold, 'filtered': filtered})
-df.to_csv('output.csv', index=False)
+# df = pd.DataFrame({'time': t, 'original': x, 'threshold': threshold, 'filtered': filtered})
+# df.to_csv('output.csv', index=False)
 
 # Plot the original and filtered signals
 fig, ax = plt.subplots(figsize=(10, 8))
 ax.plot(t, x, ':', label='Original', linewidth=2.5)
-ax.plot(t, threshold*np.ones_like(x), ':', label='Threshold', color='red')
+ax.plot(t, threshold, ':', label='Threshold', color='red')
 ax.plot(t, filtered, ':', label='Filtered', linewidth=2.5)
 ax.vlines(movement_detected, ymin=filtered.min(), ymax=filtered.max(), colors='green', label='Movement Detected')
 ax.set_ylabel('Signal')
