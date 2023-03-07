e556a85b03483a04a7d548eecb3a944ba4ef526d
jared <jach2355@gmail.com>
2023-03-08 02:03:54 +0800

    data collection preprocessing simulator v2

--- fpga/training_data_collector_simulator.py
+++ fpga/training_data_collector_simulator.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 from scipy import stats, signal
 import csv
+import os
 
 
 def preprocess_data(df):
@@ -15,7 +16,7 @@ def preprocess_data(df):
         return np.var(data)
 
     def compute_median_absolute_deviation(data):
-        return np.var(data)
+        return np.median(data)
 
     def compute_root_mean_square(data):
         return np.sqrt(np.mean(np.square(data)))
@@ -98,59 +99,89 @@ def generate_simulated_data():
     yaw = random.uniform(-180, 180)
     pitch = random.uniform(-180, 180)
     roll = random.uniform(-180, 180)
-    accx = random.uniform(-10, 10)
-    accy = random.uniform(-10, 10)
-    accz = random.uniform(-10, 10)
-    flex1 = random.uniform(0, 1023)
-    flex2 = random.uniform(0, 1023)
-    return [yaw, pitch, roll, accx, accy, accz, flex1, flex2]
+    accX = random.uniform(-1000, 1000)
+    accY = random.uniform(-1000, 1000)
+    accZ = random.uniform(-1000, 1000)
+    flex1 = random.uniform(-180, 180)
+    flex2 = random.uniform(-180, 180)
+    return [flex1, flex2, yaw, pitch, roll, accX, accY, accZ]
 
 
 if __name__ == "__main__":
-    raw_headers = ["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"]
     all_data = []
 
+    raw_headers = ['flex1', 'flex2', 'yaw', 'pitch', 'roll', 'accX', 'accY', 'accZ']
+
     # defining headers for post processing
-    variables = ['Acc-X', 'Acc-Y', 'Acc-Z', 'Gyro-X', 'Gyro-Y', 'Gyro-Z', 'Flex1', 'Flex2']
-    factors = ['mean', 'variance', 'median_absolute_deviation', 'root_mean_square', 'interquartile_range',            
+    factors = ['mean', 'variance', 'median', 'root_mean_square', 'interquartile_range',            
             'percentile_75', 'kurtosis', 'min_max', 'signal_magnitude_area', 'zero_crossing_rate',            
             'spectral_centroid', 'spectral_entropy', 'spectral_energy', 'principle_frequency']
 
-    headers = [f'{var}_{factor}' for var in variables for factor in factors]
-    # headers.insert(0, 'timestamp') # inserting timestamp at the beginning of the list
+    headers = [f'{raw_header}_{factor}' for raw_header in raw_headers for factor in factors]
+    headers.append('action')
 
     print("Start")
-    try:
-        while True:
-            # collecting data every 0.05 seconds
-            for i in range(20):
+    flag = True
+    if 1 == 1:
+        if flag:
+            flag = False
+            # collecting data upon key press and 1s sleep timer
+            print("Press any key to start data collection...\n")
+
+            i=0
+            start_time = time.time()
+            print("Recording for 2 seconds...\n")
+
+            # assuming all actions within 1 second of key press
+            while time.time() - start_time < 2:
                 data = generate_simulated_data()
-                all_data.append(data)
+                print(f"data: {data} \n")
+                # if len(data) == 0 or data[0] != "#":
+                #     print("Invalid data:", data)
+                #     continue
+
+                # data = data[1:].split(",")
+                if len(data) == 8:
+                    flex1, flex2, yaw, pitch, roll, accX, accY, accZ = data
+                    
+                    all_data.append(
+                        [flex1, flex2, yaw, pitch, roll, accX, accY, accZ]
+                    )
                 time.sleep(0.05)
+                print(f"iteration: {i}")
+                i+=1
+
 
             # Convert data to DataFrame
             df = pd.DataFrame(all_data, columns=raw_headers)
 
-            # Store raw data into a new CSV file
-            filename = time.strftime("%Y%m%d-%H%M%S") + "_raw.csv"
-            df.to_csv(filename, index=False, header=True)
-
             # Show user the data and prompt for confirmation
-            print(df.head())
+            print(df[['yaw', 'pitch', 'roll', 'accX', 'accY', 'accZ']].head(40))
+            print(f"Number of rows and columns: {df.shape[0]} by {df.shape[1]}")
+            # user_input = input("Does the data look ok? (y/n): ")
             if 1 == 1:
+
                 # Store raw data into a new CSV file
                 filename = time.strftime("%Y%m%d-%H%M%S") + "_raw.csv"
                 df.to_csv(filename, index=False, header=True)
                 
-                # Add timestamp column to processed data
-                timestamp = time.strftime("%Y%m%d-%H%M%S")
+                # process data
                 processed_data = preprocess_data(df)
+                print("processed_data: \n")
                 print(processed_data)
+                print("\n")
+
+                # Prompt user for label
+                print("Enter label (G = GRENADE, R = RELOAD, S = SHIELD, L = LOGOUT): ")
+                label = "G"
+
+                # Append label to processed data
+                processed_data = np.append(processed_data, label)
 
                 # Append processed data to CSV file
                 with open("processed_data.csv", "a") as f:
                     writer = csv.writer(f)
-                    writer.writerow(headers)
+                    writer.writerow(headers) # can remove aft file is init 
                     writer.writerow(processed_data)
 
                 # Clear raw data list
@@ -159,6 +190,3 @@ if __name__ == "__main__":
             else:
                 all_data = []
                 print("Data not processed.")
-
-    except Exception:
-        print("an error occurred")
