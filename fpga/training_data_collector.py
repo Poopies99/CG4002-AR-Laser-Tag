d8cd4ad8a20437ea70159bfa33a3587d44c7883e
jared <jach2355@gmail.com>
2023-03-08 02:00:56 +0800

    data collection and preprocessing v2

--- fpga/training_data_collector.py
+++ fpga/training_data_collector.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from intcomm import IntComm
 import time
 import numpy as np
 from scipy import stats, signal
 import csv
+import os
 
 def preprocess_data(df):
     
@@ -14,8 +14,8 @@ def preprocess_data(df):
     def compute_variance(data):
         return np.var(data)
 
-    def compute_median_absolute_deviation(data):
-        return stats.median_abs_deviation(data, axis=None)
+    def compute_median(data):
+        return np.median(data)
 
     def compute_root_mean_square(data):
         return np.sqrt(np.mean(np.square(data)))
@@ -66,7 +66,7 @@ def preprocess_data(df):
         # Compute features for the column
         mean = compute_mean(column_data)
         variance = compute_variance(column_data)
-        median_absolute_deviation = compute_median_absolute_deviation(column_data)
+        median = compute_median(column_data)
         root_mean_square = compute_root_mean_square(column_data)
         interquartile_range = compute_interquartile_range(column_data)
         percentile_75 = compute_percentile_75(column_data)
@@ -80,7 +80,7 @@ def preprocess_data(df):
         principle_frequency = compute_principle_frequency(column_data)
 
         # Store features in list
-        processed_column_data = [mean, variance, median_absolute_deviation, root_mean_square, 
+        processed_column_data = [mean, variance, median, root_mean_square, 
                                 interquartile_range, percentile_75, kurtosis, min_max, 
                                 signal_magnitude_area, zero_crossing_rate, spectral_centroid, 
                                 spectral_entropy, spectral_energy, principle_frequency]
@@ -93,71 +93,51 @@ def preprocess_data(df):
     return processed_data_arr
 
 
-# def init_csv():
-#     variables = ['Acc-X', 'Acc-Y', 'Acc-Z', 'Gyro-X', 'Gyro-Y', 'Gyro-Z', 'Flex1', 'Flex2']
-#     factors = ['mean', 'variance', 'median_absolute_deviation', 'root_mean_square', 'interquartile_range',            
-#             'percentile_75', 'kurtosis', 'min_max', 'signal_magnitude_area', 'zero_crossing_rate',            
-#             'spectral_centroid', 'spectral_entropy', 'spectral_energy', 'principle_frequency']
-
-#     headers = [f'{var}_{factor}' for var in variables for factor in factors]
-#     headers.insert(0, 'timestamp') # inserting timestamp at the beginning of the list
-
-#     # Open a new CSV file and write headers
-#     with open('filename.csv', mode='w', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(headers)
-
 if __name__ == "__main__":
-
-    # change this according to serial port
-    # 0: "/dev/ttyACM0"
-    # 1: "/dev/ttyACM1"
-    # 2: "/dev/ttyACM2"
-    intcomm = IntComm(0) # TODO comms - goal: get data from arduino
     all_data = []
 
-    raw_headers = ["yaw", "pitch", "roll", "ax", "ay", "az", "flex1", "flex2"]
+    raw_headers = ['flex1', 'flex2', 'yaw', 'pitch', 'roll', 'accX', 'accY', 'accZ']
 
     # defining headers for post processing
-    variables = ['Acc-X', 'Acc-Y', 'Acc-Z', 'Gyro-X', 'Gyro-Y', 'Gyro-Z', 'Flex1', 'Flex2']
-    factors = ['mean', 'variance', 'median_absolute_deviation', 'root_mean_square', 'interquartile_range',            
+    factors = ['mean', 'variance', 'median', 'root_mean_square', 'interquartile_range',            
             'percentile_75', 'kurtosis', 'min_max', 'signal_magnitude_area', 'zero_crossing_rate',            
             'spectral_centroid', 'spectral_entropy', 'spectral_energy', 'principle_frequency']
 
-    headers = [f'{var}_{factor}' for var in variables for factor in factors]
-    # headers.insert(0, 'timestamp') # inserting timestamp at the beginning of the list
+    headers = [f'{raw_header}_{factor}' for raw_header in raw_headers for factor in factors]
+    headers.append('action')
 
     print("Start")
-    try:
+    if 1 == 1:
         while True:
             # collecting data upon key press and 1s sleep timer
-            input("Press any key to start data collection...")
-            time.sleep(1)
+            input("Press any key to start data collection...\n")
 
             start_time = time.time()
-            print("Recording for 1 second...")
+            print("Recording for 2 seconds...\n")
 
             # assuming all actions within 1 second of key press
-            while time.time() - start_time < 1:
-                data = intcomm.get_line() # TODO comms - goal: get data from arduino
-                print(data)
-                if len(data) == 0 or data[0] != "#":
-                    print("Invalid data:", data)
-                    continue
+            while time.time() - start_time < 2:
+                data = ... # TODO comms - refactor to call in data
+                print(f"data: {data} \n")
+                # if len(data) == 0 or data[0] != "#":
+                #     print("Invalid data:", data)
+                #     continue
 
-                data = data[1:].split(",")
+                # data = data[1:].split(",")
                 if len(data) == 8:
-                    yaw, pitch, roll, accx, accy, accz, flex1, flex2 = data
+                    flex1, flex2, yaw, pitch, roll, accX, accY, accZ = data
                     
                     all_data.append(
-                        [yaw, pitch, roll, accx, accy, accz, flex1, flex2]
+                        [flex1, flex2, yaw, pitch, roll, accX, accY, accZ]
                     )
 
             # Convert data to DataFrame
             df = pd.DataFrame([all_data], columns=raw_headers)
 
             # Show user the data and prompt for confirmation
-            print(df.head())
+            print(df[['yaw', 'pitch', 'roll', 'accX', 'accY', 'accZ']].head(40))
+            print(f"Number of rows and columns: {df.shape[0]} by {df.shape[1]}")
+
             user_input = input("Does the data look ok? (y/n): ")
             if user_input.lower() == "y":
 
@@ -165,15 +145,22 @@ if __name__ == "__main__":
                 filename = time.strftime("%Y%m%d-%H%M%S") + "_raw.csv"
                 df.to_csv(filename, index=False, header=True)
                 
-                # Add timestamp column to processed data
-                # timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+                # process data
                 processed_data = preprocess_data(df)
+                print("processed_data: \n")
                 print(processed_data)
+                print("\n")
+
+                # Prompt user for label
+                label = input("Enter label (G = GRENADE, R = RELOAD, S = SHIELD, L = LOGOUT): ")
+
+                # Append label to processed data
+                processed_data = np.append(processed_data, label)
 
                 # Append processed data to CSV file
                 with open("processed_data.csv", "a") as f:
                     writer = csv.writer(f)
-                    writer.writerow(headers)
+                    writer.writerow(headers) # can remove aft file is init 
                     writer.writerow(processed_data)
 
                 # Clear raw data list
@@ -182,7 +169,4 @@ if __name__ == "__main__":
             else:
                 print("Data not processed.")
 
-    except KeyboardInterrupt:
-        print("terminating program")
-    except Exception:
-        print("an error occured")
+
