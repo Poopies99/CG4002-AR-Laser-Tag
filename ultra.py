f692328551f271b1e6cb1974bf53d3e814ea4f75
Jared <jach2355@gmail.com>
2023-04-07 20:57:28 +0800

    ai debug

--- ultra.py
+++ ultra.py
@@ -879,8 +879,7 @@ if __name__ == '__main__':
 
     # # Server Connection to Laptop
     print("Starting Server Thread")
-    laptop_server = Server(constants.XILINX_PORT_NUM, constants.XILINX_SERVER)
-    
+    laptop_server = Server(constants.XILINX_PORT_NUM, constants.XILINX_SERVER, [])
 #     laptop_server = Server(constants.XILINX_PORT_NUM, constants.XILINX_SERVER, action_engine)
 
     print('--------------------------------------------------')
